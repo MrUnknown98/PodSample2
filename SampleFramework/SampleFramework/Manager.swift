8b29c6a9448afffc1f6e9ea91a6cf077313a802d
Sakthivel Mani <sakthimani154@gmail.com>
2021-10-04 10:30:46 +0530

    Framework Commit

---
+++ SampleFramework/SampleFramework/Manager.swift
@@ -0,0 +1,18 @@
+//
+//  Manager.swift
+//  SampleFramework
+//
+//  Created by Sakthivel on 04/10/21.
+//
+
+import Foundation
+
+public class Manager {
+    public init() {
+        
+    }
+    
+    public func sayHello() {
+        print("Hello Sakthi")
+    }
+}
